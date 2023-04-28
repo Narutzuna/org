f32645efad34bc239e7f89662e35384cac5fff86
Narutzuna <gibraneduardo523@gmail.com>
2023-04-28 03:04:37 -0600

    componentes grupo

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Formulario from './components/Formulario/Formulario.js';
 import './App.css';
 import Header from './components/Headers/Header';
 import MiOrg from './components/MiOrg/index.js';
+import Equipo from './components/Equipo/index.js';
 function App() {
   
   const [mostrarFormulario, actulizarMostrar] = useState(true)
@@ -10,6 +11,52 @@ function App() {
   const cambiarMostrar = () =>{
     actulizarMostrar(!mostrarFormulario)
   } 
+
+  // Mi lista de equipos
+
+  const equipos = [
+    {
+      titulo:"Programació", 
+      colorPrimario: '#57C278',
+      colorSecundario: '#D9F7E9',
+    },
+
+    {
+      titulo:"Front End", 
+      colorPrimario: '#82CFFA',
+      colorSecundario: '#E8F8FF',
+    },
+
+    {
+      titulo:"Data Science", 
+      colorPrimario: '#A6D157',
+      colorSecundario: '#F0F8E2',
+    },
+
+    {
+      titulo:"Devops", 
+      colorPrimario: '#E06B69',
+      colorSecundario: '#FDE7E8',
+    },
+
+    {
+      titulo:"UX y Diseño", 
+      colorPrimario: '#DB6EBF',
+      colorSecundario: '#FAE9F5',
+    },
+
+    {
+      titulo:"Móvil", 
+      colorPrimario: '#FFBA05',
+      colorSecundario: '#FFF5D9',
+    },
+
+    {
+      titulo:"Innovación y Gestión", 
+      colorPrimario: '#FF8A29',
+      colorSecundario: '#FFEEDF',
+    },
+]
   
   // Ternario --> condicion ? seMuestra : noSeMuestra
   // Corto Circuito ----> Condicion && seMuestra 
@@ -20,6 +67,14 @@ function App() {
       { /*mostrarFormulario /*=== true ? <Formulario /> : <></>*/}
       {mostrarFormulario && <Formulario/>}
       <MiOrg cambiarMostrar={cambiarMostrar}/>
+      {
+        equipos.map( (equipo) => {
+          console.log('Equipo:', equipo)
+          return <Equipo datos={equipo} key={equipo.titulo}/>
+        } )
+      
+      }
+
     </div>
   );
 }
